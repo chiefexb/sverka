bc7882845ea18a8e6faca2b3acc2b713de388c99
chiefexb <chief_exb@mail.ru>
2016-03-05 19:27:17 +0300

    Может обрабатывать не все базы а на выбор из коммандной строки

--- sverka.py
+++ sverka.py
@@ -36,26 +36,31 @@ def main():
  dbm=[]
  #выбираем годы
  if len(sys.argv)<=1:
-  print ("getfromint: нехватает параметров\nИспользование: sverka.py sverka 2007,2008")
+  print ("getfromint: нехватает параметров\nИспользование: sverka.py sverka d2007,d2008")
+  print "sverka.py sverka all по всем годам"
   sys.exit(2)
  print sys.argv[2],sys.argv[3]
  dbm=[]
+ if sys.argv[2]=='all':
+  years=['d2007','d2008','d2009','d2010','d2011']
+ else:
+  years=sys.argv[2].split(',')
+ print years
  for d  in xmlroot.getchildren():
   print d.tag
-  #dbs={}
-  #dbmass={}
   #выбираем базы за год
-  #dbs['year']=d.tag
-  print len(d.getchildren())
-  for a in d.getchildren():
-   print a.tag
-   #перебор атрибутов
-   dbs={}
-   dbs['year']=d.tag
-   dbs['alias']=a.tag
-   for itms in a.attrib.items():
-    dbs[itms[0]]=itms[1]
-   dbm.append(dbs)
+  if d.tag in years:
+   #dbs['year']=d.tag
+   print len(d.getchildren())
+   for a in d.getchildren():
+    print a.tag
+    #перебор атрибутов
+    dbs={}
+    dbs['year']=d.tag
+    dbs['alias']=a.tag
+    for itms in a.attrib.items():
+     dbs[itms[0]]=itms[1]
+    dbm.append(dbs)
  print dbm,len(dbm)
  print 'TEST CONN'
  i=0
@@ -64,6 +69,10 @@ def main():
   print i,pp['year'],pp['alias'],pp['db']
   try:
    con = fdb.connect (host=pp['host'], database=pp['db'], user='SYSDBA', password=pp['password'],charset='WIN1251')
+   cur=con.cursor()
+   cur.execute ('select count(pk) from id')
+   r=cur.fetchone()
+   print r
    con.close()
    print  pp['host'],pp['db'],'OK' 
   except  Exception, e:
